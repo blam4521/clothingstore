a3d8b01be6ea59679f778ad35d424022f9629bbb
Barry Lam <blam452@gmail.com>
2019-09-15 14:09:03 -0400

    [BX-0001] Add header and preview pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
